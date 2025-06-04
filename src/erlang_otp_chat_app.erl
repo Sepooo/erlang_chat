8781b3922326ef64647982eee04a23bca908783f
Sepooo <nicolopriano23@gmail.com>
2025-06-04 14:07:04 +0200

    Start app

--- src/erlang_otp_chat_app.erl
+++ src/erlang_otp_chat_app.erl
@@ -11,6 +11,7 @@
 
 start(_StartType, _StartArgs) ->
     erlang_otp_chat_sup:start_link(), % Starts main supervisor
+    erlang_otp_chat_sup:start_tcp_worker(undefined, {0,0,0,0}, 8080, []),
     room_registry:start_link(). % Initialize ETS for rooms
 
 stop(_State) ->
